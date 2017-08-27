436936b5394515025782913512a6da5fffc21e02
ralusek <ralusek@gmail.com>
2017-08-26 17:26:58 -0700

    added performance gains by replacing forEach with for loop.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-'use strict';
-
-
 const memoize = (...selectors) => {
   const compute = selectors.pop();
   // If no initial selector is provided other than compute, assume we just
@@ -8,14 +5,19 @@ const memoize = (...selectors) => {
   if (!selectors.length) selectors.push(state => state);
   let previousSelectorResults = [];
   let previousComputeResult;
+
+  const len = selectors.length;
   
-  return (state) => {
+  return function() {
     const current = [];
     let unchanged = true;
-    selectors.forEach((selector, i) => ((current[i] = selector(state)) !== previousSelectorResults[i]) && (unchanged = false));
+    for (let i = 0; i < len; i++) {
+      ((current[i] = selectors[i].apply(null, arguments)) !== previousSelectorResults[i]) && (unchanged = false)
+    }
+
     previousSelectorResults = current;
     if (unchanged) return previousComputeResult;
-    return previousComputeResult = compute(...current);
+    return previousComputeResult = compute.apply(null, current);
   }
 };
 
